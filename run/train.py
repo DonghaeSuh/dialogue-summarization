791bff85565f4b268e2df6b79fa88835f277953d
DonghaeSuh <gypsi12@naver.com>
2024-07-17 21:14:12 +0900

    fix - cpu memory 초과 문제 해결

--- run/train.py
+++ run/train.py
@@ -10,6 +10,9 @@ from transformers import (AutoModelForCausalLM,
                           EarlyStoppingCallback)
 import pandas as pd
 import numpy as np
+import gc
+import evaluate
+
 from rouge import Rouge
 
 from peft import prepare_model_for_kbit_training, LoraConfig, get_peft_model
@@ -33,8 +36,8 @@ g.add_argument("--batch_size", type=int, default=1, help="batch size (both train
 g.add_argument("--gradient_accumulation_steps", type=int, default=1, help="gradient accumulation steps")
 g.add_argument("--warmup_steps", type=int, help="scheduler warmup steps")
 g.add_argument("--lr", type=float, default=2e-5, help="learning rate")
-g.add_argument("--epoch", type=int, default=5, help="training epoch")
-g.add_argument("--metric", type=str, default='rouge', help="metric for evaluation (eval_loss or rouge)")
+g.add_argument("--epoch", type=int, default=10, help="training epoch")
+g.add_argument("--metric", type=str, default='rouge1', help="metric for evaluation (eval_loss or rouge)")
 # fmt: on
 
 def print_trainable_parameters(model):
@@ -88,8 +91,8 @@ def main(args):
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
     tokenizer.pad_token = tokenizer.eos_token
 
-    train_dataset = CustomDataset("resource/data/일상대화요약_train.json", tokenizer)
-    valid_dataset = CustomDataset("resource/data/일상대화요약_dev.json", tokenizer)
+    train_dataset = CustomDataset("resource/data/일상대화요약_train.json", tokenizer)
+    valid_dataset = CustomDataset("resource/data/일상대화요약_dev.json", tokenizer)
 
     train_dataset = Dataset.from_dict({
         'input_ids': train_dataset.inp,
@@ -101,40 +104,44 @@ def main(args):
         })
     data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer)
 
+    rouge = evaluate.load("rouge")
+    # rouge = Rouge()
 
-    ## Postprocess ##
     def postprocess_text(preds, labels):
         preds = [pred.strip() for pred in preds]
         labels = [label.strip() for label in labels]
 
-    ## Metric ##
-    def compute_rouge_f1(predictions, labels):
-        rouge = Rouge()
-
-        # remove padding tokens (-100)
-        predictions = np.array(predictions)
-        labels = np.array(labels)
-
-        labels = labels[labels != -100]
-        predictions = predictions[labels != -100]
-
-        # Some simple post-processing
-        predictions, labels = postprocess_text(predictions, labels)
-
-        # decode predictions and labels
-        predictions = tokenizer.batch_decode(predictions, skip_special_tokens=True)
-        labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-
-        rouge_scores = rouge.get_scores(predictions, labels, avg=True)
-        return round(rouge_scores['rouge-1']['f'], 4)
-
+        return preds, labels
 
     def compute_metrics(eval_pred: EvalPrediction):
         # compute Rouge-1 F1 score
-        predictions = eval_pred.predictions # (batch_size, seq_len)
         labels = eval_pred.label_ids # (batch_size, seq_len)
+        predictions = eval_pred.predictions[0].reshape(labels.shape[0],-1) # (batch_size, seq_len)
 
-        return {'rouge-1': compute_rouge_f1(predictions, labels)}
+        # Replace -100 with pad_token_id
+        mask  = np.where(labels == -100)
+        labels[mask] = tokenizer.pad_token_id
+        predictions[mask] = tokenizer.pad_token_id
+
+        # Decoding
+        predictions = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+        labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+        # Simple postprocessing
+        predictions, labels = postprocess_text(predictions, labels)
+
+        rouge_scores = rouge.compute(predictions=predictions, references=labels, rouge_types=["rouge1"])
+        # rouge_scores = rouge.get_scores(predictions, labels, avg=True)
+
+        return {k: round(v, 4) for k, v in rouge_scores.items()}
+
+    def preprocess_logits_for_metrics(logits, labels):
+      """
+      Original Trainer may have a memory leak. 
+      This is a workaround to avoid storing too many tensors that are not needed.
+      """
+      pred_ids = torch.argmax(logits, dim=-1)
+      return pred_ids, labels
 
     training_args = SFTConfig(
         output_dir=args.save_dir,
@@ -145,14 +152,13 @@ def main(args):
         eval_strategy="steps", # for early stopping
         eval_steps=5, # for early stopping
         metric_for_best_model = args.metric, # for early stopping
-        compute_metrics=compute_metrics,
         load_best_model_at_end = True, # for early stopping
-        callbacks = [EarlyStoppingCallback(early_stopping_patience=3)], # for early stopping
         greater_is_better = True, # for early stopping. If true, the best model is the one with the highest value of the metric (used for rouge)
         
         per_device_train_batch_size=args.batch_size,
         per_device_eval_batch_size=args.batch_size,
         gradient_accumulation_steps=args.gradient_accumulation_steps,
+        eval_accumulation_steps=4,
         learning_rate=args.lr,
         weight_decay=0.1,
 
@@ -164,7 +170,7 @@ def main(args):
         log_level="info",
         logging_steps=1,
         save_strategy="steps",
-        save_total_limit=3,
+        save_total_limit=4,
 
         bf16=True,
         gradient_checkpointing=True,
@@ -175,7 +181,7 @@ def main(args):
 
         seed=42,
         report_to="wandb",
-        run_name=args.model_id,
+        run_name=args.model_id.split('/')[-1],
     )
 
     trainer = SFTTrainer(
@@ -185,6 +191,9 @@ def main(args):
         eval_dataset=valid_dataset,
         data_collator=data_collator,
         args=training_args,
+        compute_metrics=compute_metrics,
+        callbacks = [EarlyStoppingCallback(early_stopping_patience=3)], # for early stopping
+        preprocess_logits_for_metrics = preprocess_logits_for_metrics
     )
 
     trainer.train()
