429ad7d418c12ba9c31cd56fad8278f597bab269
HayeonLee88 <hayeon0808@swu.ac.kr>
2024-07-20 12:18:41 +0900

    chore-moditfy recipe
    -hyeogi/Yi-6b-dpo-v0.2 모델에 맞는 하이퍼파라미터 recipe

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-CUDA_VISIBLE_DEVICES=0 python -m run.train \
-    --model_id {model id} \
-    --batch_size {batch size} \
-    --gradient_accumulation_steps {data batch size} \
-    --epoch {epoch} \
+CUDA_VISIBLE_DEVICES=0 python -m run.qlora_train \
+    --model_id hyeogi/Yi-6b-dpo-v0.2 \
+    --batch_size 1 \
+    --gradient_accumulation_steps 64 \
+    --epoch 20 \
     --lr 2e-5 \
-    --warmup_steps 20
+    --warmup_steps 20 \
+    --save_dir ./resource/results/Yi-6b-dpo-v0.2/ \
